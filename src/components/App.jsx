117d14dfb2915189a2adeafb0392bc5c95423440
makswelll <nikolaevic21.maks@gmail.com>
2023-11-17 21:05:05 +0200

    up hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 
 import { ContactForm } from './ContactForm/ContactForm';
@@ -6,48 +6,45 @@ import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { GlobalStyle } from './GlobalStyle';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-  componentDidMount() {
+export const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
     const contactsList = JSON.parse(localStorage.getItem('ContactsList'));
     if (contactsList) {
-      this.setState({ contacts: contactsList });
+      setContacts(contactsList);
     }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('ContactsList', JSON.stringify(this.state.contacts));
-    }
-  }
+  }, []);
 
-  handleChangeFilter = e => {
-    this.setState({ filter: e.target.value });
+  useEffect(() => {
+    localStorage.setItem('ContactsList', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleChangeFilter = e => {
+    setFilter(e.target.value);
   };
 
-  getFilteredContacts = () => {
-    const { contacts, filter } = this.state;
+  const getFilteredContacts = () => {
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
   };
 
-  handleAddContact = (name, number) => {
+  const handleAddContact = (name, number) => {
     const normalizedName = name.toLowerCase();
-    const isNameExists = this.state.contacts.some(
+    const isNameExists = contacts.some(
       contact => contact.name.toLowerCase() === normalizedName
     );
     if (isNameExists) {
@@ -55,24 +52,23 @@ export class App extends Component {
       return;
     }
 
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, { id: `id-${nanoid()}`, name, number }],
-    }));
+    setContacts(prevContacts => [
+      ...prevContacts,
+      { id: `id-${nanoid()}`, name, number },
+    ]);
   };
 
-  render() {
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm onAddContact={this.handleAddContact} />
-        <h2>Contacts</h2>
-        <Filter value={this.state.filter} onChange={this.handleChangeFilter} />
-        <ContactList
-          contacts={this.getFilteredContacts()}
-          onDeleteContact={this.deleteContact}
-        />
-        <GlobalStyle />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onAddContact={handleAddContact} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={handleChangeFilter} />
+      <ContactList
+        contacts={getFilteredContacts()}
+        onDeleteContact={deleteContact}
+      />
+      <GlobalStyle />
+    </div>
+  );
+};
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,49 +1,45 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { FormButton, FormStyle } from './ContactFormStyled';
 
-export class ContactForm extends Component {
-  state = { name: '', number: '' };
+export const ContactForm = ({ onAddContact }) => {
+  const [contact, setContact] = useState({ name: '', number: '' });
 
-  handleChange = e => {
+  const handleChange = e => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
+    setContact(prevContact => ({ ...prevContact, [name]: value }));
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    const { name, number } = this.state;
+    const { name, number } = contact;
 
     if (name === '' || number === '') {
       alert('Please enter both name and phone number for the contact.');
       return;
     }
 
-    this.props.onAddContact(name, number);
-
-    this.setState({ name: '', number: '' });
+    onAddContact(name, number);
+    setContact({ name: '', number: '' });
   };
-
-  render() {
-    return (
-      <FormStyle onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          value={this.state.name}
-          onChange={this.handleChange}
-          required
-          placeholder="Enter name"
-        />
-        <input
-          type="text"
-          name="number"
-          value={this.state.number}
-          onChange={this.handleChange}
-          required
-          placeholder="Enter phone number"
-        />
-        <FormButton type="submit">Add contact</FormButton>
-      </FormStyle>
-    );
-  }
-}
+  return (
+    <FormStyle onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="name"
+        value={contact.name}
+        onChange={handleChange}
+        required
+        placeholder="Enter name"
+      />
+      <input
+        type="text"
+        name="number"
+        value={contact.number}
+        onChange={handleChange}
+        required
+        placeholder="Enter phone number"
+      />
+      <FormButton type="submit">Add contact</FormButton>
+    </FormStyle>
+  );
+};
