5ce746a6d3cf84fb543ddb4a2b268c292f6361db
makswelll <nikolaevic21.maks@gmail.com>
2023-11-19 19:11:04 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,21 +6,15 @@ import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { GlobalStyle } from './GlobalStyle';
 
+const inialState = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(inialState);
   const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    const contactsList = JSON.parse(localStorage.getItem('ContactsList'));
-    if (contactsList) {
-      setContacts(contactsList);
-    }
-  }, []);
 
   useEffect(() => {
     localStorage.setItem('ContactsList', JSON.stringify(contacts));
